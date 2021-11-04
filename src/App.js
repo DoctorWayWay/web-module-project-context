aa41a79168e263b9a06d6ad9843b4815e062d720
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-11-04 14:16:35 -0700

    Wrapped app in with CardContext.Provider.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import data from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+
+// Contexts
 import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
@@ -19,18 +22,18 @@ function App() {
 
   return (
     <ProductContext.Provider value={{ products, addItem }}>
-      <div className="App">
-        <Navigation cart={cart} />
-
-        {/* Routes */}
-        <Route exact path="/">
-          <Products />
-        </Route>
-
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </div>
+      <CartContext.Provider value={{ cart }}>
+        <div className="App">
+          <Navigation cart={cart} />
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
+          <Route path="/cart">
+            <ShoppingCart cart={cart} />
+          </Route>
+        </div>
+      </CartContext.Provider>
     </ProductContext.Provider>
   );
 }
